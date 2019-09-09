609f35cef56d0022e7ba59a20b7c50a0d1c22bad
Cuttineta <54332299+Cuttineta@users.noreply.github.com>
2019-09-09 13:41:56 -0300

    Implementacion de aliado

---
+++ src/juego/Aliado.java
@@ -0,0 +1,21 @@
+package juego;
+
+public class Aliado extends Personaje {
+
+	private int vida;
+	private int alcance;
+	private int impacto;
+
+	public int getVida() {
+		return vida;
+	}
+
+	public int getAlcance() {
+		return alcance;
+	}
+
+	public int getImpacto() {
+		return impacto;
+	}
+
+}
