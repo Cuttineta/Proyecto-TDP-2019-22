6f1bf73ed1ad86b6267e37d2ce9a95137aea4b2f
Aitor <aitorortunio@gmail.com>
2019-09-09 16:35:45 -0300

    Updates

--- src/Mapa/GUI.java
+++ src/Mapa/GUI.java
@@ -13,15 +13,13 @@ public class GUI extends Canvas {
 	private static final int ANCHO = 800;
 	private static final int ALTO = 600;
 
-	private static final String NOMBRE = "Juego";
-
 	private static JFrame ventana;
 
 	private GUI() {
 
 		setPreferredSize(new Dimension(ANCHO, ALTO));
 
-		ventana = new JFrame(NOMBRE);
+		ventana = new JFrame("Juego");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setResizable(false);
 		ventana.setLayout(new BorderLayout());
