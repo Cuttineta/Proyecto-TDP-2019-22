436a56c5c4ba21e20ef580dee95455b839f6b632
Cuttineta <54332299+Cuttineta@users.noreply.github.com>
2019-09-10 17:04:17 -0300

    .

--- src/Mapa/GUI.java
+++ src/Mapa/GUI.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class GUI extends Canvas implements Runnable{
+public class GUI extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -15,18 +17,24 @@ public class GUI extends Canvas implements Runnable{
 
 	private static JFrame ventana;
 	private static Thread thread;
+	private JLabel fondo;
 	private static volatile boolean funcionamiento = false;
 
 	private GUI() {
+		setBounds(0, 0, 1024, 700);
 		setPreferredSize(new Dimension(ANCHO, ALTO));
 		ventana = new JFrame("Juego");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setResizable(false);
-		ventana.setLayout(new BorderLayout());
-		ventana.add(this, BorderLayout.CENTER);
+		ventana.getContentPane().setLayout(new BorderLayout());
+		ventana.getContentPane().add(this);
 		ventana.pack();
 		ventana.setLocationRelativeTo(null);
 		ventana.setVisible(true);
+		/*fondo.setIcon(new ImageIcon("imágenes\\Sprites\\asfalto good.png"));
+		fondo.setPreferredSize(new Dimension(ANCHO, ALTO - 100));
+		ventana.getContentPane().add(fondo);*/
+
 	}
 
 	public static final void main(String args[]) {
@@ -39,15 +47,15 @@ public class GUI extends Canvas implements Runnable{
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	private synchronized void finish() {
 		funcionamiento = false;
 	}
-	
+
 	@Override
 	public void run() {
-		while(funcionamiento) {
-			
+		while (funcionamiento) {
+
 		}
 	}
 
