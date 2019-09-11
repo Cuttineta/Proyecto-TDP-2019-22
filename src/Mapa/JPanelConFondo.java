57c56d3c61a97e2fab6f3e65baa2ff2c2a7b6ab0
Aitor <aitorortunio@gmail.com>
2019-09-11 18:41:11 -0300

    new background system

---
+++ src/Mapa/JPanelConFondo.java
@@ -0,0 +1,18 @@
+package Mapa;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+ 
+public class JPanelConFondo extends JPanel { 
+ 
+    private Image imagen= new ImageIcon("src/Sprites/asfalto.jpg").getImage();
+
+    public void paint(Graphics g) {
+        g.drawImage(imagen, 0, 0, getWidth(), getHeight(),this);
+        setOpaque(false);
+        super.paint(g);
+    }
+}
