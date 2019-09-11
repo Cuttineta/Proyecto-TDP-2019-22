43cfcfa6251bf078cc5a1d731961b925ce217d15
Cuttineta <54332299+Cuttineta@users.noreply.github.com>
2019-09-11 11:36:29 -0300

    Update GUI.java

--- src/Mapa/GUI.java
+++ src/Mapa/GUI.java
@@ -3,38 +3,55 @@ package Mapa;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
-import javax.swing.JFrame;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
-public class GUI extends Canvas implements Runnable {
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GUI extends JFrame implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
 	private static final int ANCHO = 1024;
 	private static final int ALTO = 700;
 
-	private static JFrame ventana;
+	private JLabel fondo;
+	private JPanel contentPane;
 	private static Thread thread;
 	private static volatile boolean funcionamiento = false;
 
 	public GUI() {
-		setBounds(0, 0, 1024, 700);
-		setPreferredSize(new Dimension(ANCHO, ALTO));
-		ventana = new JFrame("Juego");
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setResizable(false);
-		ventana.setLayout(new BorderLayout());
-		// ventana.add(this, BorderLayout.CENTER);
-		ventana.getContentPane().setLayout(new BorderLayout());
-		ventana.getContentPane().add(this);
-		ventana.pack();
-		ventana.setLocationRelativeTo(null);
-		ventana.setVisible(true);
 
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, ANCHO, ALTO);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		agregarDibujo();
 	}
 
 	public static final void main(String args[]) {
 		GUI gui = new GUI();
+		gui.setVisible(true);
+		gui.setResizable(false);
 		gui.start();
+	}
+
+	private void agregarDibujo() {
+		ImageIcon imagen = new ImageIcon(this.getClass().getResource("/imágenes/Sprites/asfalto good.png"));
+		fondo = new JLabel(imagen);
+
+		fondo.setBounds(0, 0, ANCHO, ALTO);
+
+		this.add(fondo);
 	}
 
 	private synchronized void start() {
