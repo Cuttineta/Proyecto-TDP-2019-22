04a2f99c605e13a12d48b27536a369ed83b169bd
Aitor <aitorortunio@gmail.com>
2019-09-24 15:25:08 -0300

    Update Enemigos

---
+++ src/Enemigo/Surtur.java
@@ -0,0 +1,57 @@
+package Enemigo;
+
+import Mapa.Celda;
+
+public class Surtur extends Enemigo {
+	
+	private static final int ALCANCE = 1, IMPACTO = 20;
+
+	public Surtur(Celda c) {
+		super(c);
+		alcance = ALCANCE;
+		impacto = IMPACTO;
+		atacando = false;
+	}
+
+	@Override
+	public int getPuntos() {
+		return puntos;
+	}
+
+	@Override
+	public int getMonedas() {
+		return monedas;
+	}
+
+	@Override
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	@Override
+	public boolean atacando() {
+		return atacando;
+	}
+
+	@Override
+	public int getVida() {
+		return vida;
+	}
+
+	@Override
+	public int getImpacto() {
+		return impacto;
+	}
+
+	@Override
+	public int getAlcance() {
+		return alcance;
+	}
+
+	@Override
+	public int getValor() {
+		return valor;
+	}
+	
+
+}
